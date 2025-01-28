ea37ef4f3800a65a3cd553b80a1a57c6920a287f
Brandon Karchewski <brandon.karchewski@ucalgary.ca>
2025-01-28 09:34:42 -0700

    added data and expected arrays for polynomial data

--- examples/test_interpolation.py
+++ examples/test_interpolation.py
@@ -12,15 +12,20 @@ def main():
     # plot the results
     import numpy as np
 
-    xd = [-5,5]
-    xd_2 = [-5,0,5]
-    xd_3 = [-5,-2,2,5]
+    xd = [-5, 5]
+    xd_2 = [-5, 0, 5]
+    xd_3 = [-5, -2, 2, 5]
 
     x = np.linspace(-10, 10, 100)
 
-    y_linear = 3 * xd + 1
-    y_quad = 3 * xd_2 ** 2 + 2 * xd_2 + 2
-    y_cubic = 3 * xd_3 ** 3 + 2 * xd_3 ^ 2 + 2 * xd_3 + 3
+    y_linear_data = 3 * xd + 1
+    y_linear_exp = 3 * x + 1
+
+    y_quad_data = 3 * xd_2**2 + 2 * xd_2 + 2
+    y_quad_exp = 3 * x**2 + 2 * x + 2
+
+    y_cubic_data = 3 * xd_3**3 + 2 * xd_3**2 + 2 * xd_3 + 3
+    y_cubic_exp = 3 * x**3 + 2 * x**2 + 2 * x + 3
 
     L_linear = interp_lagrange(x, xd, y_linear)
     f_linear = y_linear(x)
@@ -28,12 +33,12 @@ def main():
     L_quad = interp_lagrange(x, xd_2, y_quad)
     f_quad = y_quad(x)
 
-    L_cubic = interp_lagrange(x ,xd_3, y_cubic)
+    L_cubic = interp_lagrange(x, xd_3, y_cubic)
     f_cubic = y_cubic(x)
 
-    plt.plot(x, y_linear, label="Linear")
-    plt.plot(x, y_quad, label="Quadratic")
-    plt.plot(x, y_cubic, label="Cubic")
+    plt.plot(x, y_linear_exp, "-b", label="Linear")
+    plt.plot(x, y_quad_exp, "--b", label="Quadratic")
+    plt.plot(x, y_cubic_exp, "-.b", label="Cubic")
     plt.show()
 
     pass
