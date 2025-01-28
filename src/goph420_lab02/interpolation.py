7b4c2b01540274287b7eab64b831606a74f6f935
Sydney Pasloski <sydney.pasloski@ucalgary.ca>
2025-01-28 09:18:15 -0700

    created lagrange interpolation function

--- src/goph420_lab02/interpolation.py
+++ src/goph420_lab02/interpolation.py
@@ -18,4 +18,26 @@ def interp_lagrange(x, xd, fd):
     f : float or array_like
         The interpolated data. Same type and shape as x.
     """
-    pass
+
+    xd = np.asarray(xd, dtype=float)
+    n = len(xd) - 1
+    j = 0
+    k = n
+
+    def l_fraction(x, xd):
+        """Calculate Lagrange polynomial"""
+        l_frac = 1
+        for j in range(len(xd)):
+            if j == k:
+                continue
+            else:
+                l_frac *= (x - xd[j]) / (xd[k] - xd[j])
+        return l_frac
+
+    def polynomial_value(x, xd, fd):
+        f = 0
+        for i in range(len(xd)):
+            f += fd[i]*l_fraction(x, xd)
+        return f
+
+    return polynomial_value(x, xd, fd)
