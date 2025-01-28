a74f11604d38afc7ba34c5bedbc0b349302cc3d7
Viktor Pejic <viktor.pejic@ucalgary.ca>
2025-01-28 09:45:41 -0700

    update

--- examples/test_interpolation.py
+++ examples/test_interpolation.py
@@ -24,23 +24,23 @@ def main():
     y_cubic = 3 * xd_3 ** 3 + 2 * xd_3 ** 2 + 2 * xd_3 + 3
 
     L_linear = interp_lagrange(x, xd, y_linear)
-    f_linear = y_linear(x)
+
 
     L_quad = interp_lagrange(x, xd_2, y_quad)
-    f_quad = y_quad(x)
+
 
     L_cubic = interp_lagrange(x ,xd_3, y_cubic)
-    f_cubic = y_cubic(x)
 
-    plt.plot( x, f_linear, label="Linear Function")
+
+
     plt.plot(x, L_linear, label="Linear Interpolation")
     plt.scatter(xd, y_linear)
 
-    plt.plot(x, f_quad, label="Quadratic Function")
+
     plt.plot(x, L_quad, label="Quadratic Interpolation")
     plt.scatter(xd_2, y_quad)
 
-    plt.plot(x, f_cubic, label="Cubic Function")
+
     plt.plot(x, L_cubic, label="Cubic Interpolation")
     plt.plot(xd, y_cubic)
     plt.show()
