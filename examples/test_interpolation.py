f01dcabefa8c2d7d320f3577544de77017fc4fc7
Brandon Karchewski <brandon.karchewski@ucalgary.ca>
2025-01-28 08:20:08 -0700

    created template/interface for lagrange interpolation function

---
+++ examples/test_interpolation.py
@@ -0,0 +1,16 @@
+from goph420_lab02.interpolation import (
+    interp_lagrange,
+)
+
+
+def main():
+    # generate some data from polynomials of different order
+
+    # test the interp_lagrange() function
+
+    # plot the results
+    pass
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/goph420_lab02/interpolation.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+
+def interp_lagrange(x, xd, fd):
+    """Interpolate using Lagrange Interpolating Polynomials.
+
+    Parameters
+    ----------
+    x : float or array_like
+        The target point(s) for interpolation.
+    xd : array_like
+        The independent variable data.
+    fd : array_like
+        The dependent variable data.
+
+    Returns
+    -------
+    f : float or array_like
+        The interpolated data. Same type and shape as x.
+    """
+    pass
