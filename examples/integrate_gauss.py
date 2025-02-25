5fe9ef5ef94d7ad0c98b21c830e91b63e5d27dde
DeclanFitzpatrick <declan.fitzpatrick@ucalgary.ca>
2025-02-25 09:03:45 -0700

    function part. written

--- examples/integrate_gauss.py
+++ examples/integrate_gauss.py
@@ -12,12 +12,11 @@ T = [22.8, 22.8, 22.8, 20.6, 13.9, 11.7, 11.1, 11.1, 11.1]
 z = [0, 2.3, 4.9, 9.1, 13.7, 18.3, 22.9, 26.0, 27.2]
 
 
-def points(T):
-    Tlist =  []
-    for T,z in enumerate(T):
-        Tpoints = np.linspace([z][T], z[T + 2], 10)
-        Tlist.append(Tpoints)
-    return Tlist
+def nd_ord():
+    zplot = []
+    Tplot = []
+    for k, Tk in enumerate(T[:-1:2]):
+        Tloc = T
 
 def main():
     #  plotting T(z)
@@ -28,7 +27,6 @@ def main():
     plt.ylim(z[-1], z[0])
     ax.tick_params(top=True, labeltop=True, bottom=False, labelbottom=False)
     ax.set_title('z')
-
     plt.show()
 
     plt.figure()
@@ -36,8 +34,7 @@ def main():
     plt.ylabel("T(z)")
     plt.legend()
     plt.show()
-    plt.savefig("figures/Z_VS_T.png") 
-    
+
 def integral_gauss():
     #  integrate T(z) using Gaussian quadrature
     #  using 2 points
@@ -55,7 +52,6 @@ def integral_gauss():
 
 
 
-    
 
 if __name__ == "__main__":
     main()
